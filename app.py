1f683e8d3e612c549cef69e1d9bf5665164bf08c
pelobravo <pelobravo@gmail.com>
2026-06-19 18:33:33 -0400

    app.py

--- app.py
+++ app.py
@@ -22,6 +22,11 @@ validar_carpetas()
 db = Database()
 logger = Logger()
 
+# 🔥 TEMPORAL: Botón para LIMPIAR TODOS los saldos
+if st.button("🗑️ LIMPIAR SALDOS"):
+    db.limpiar_saldos()
+    st.success("✅ Tabla saldos_diarios limpiada correctamente")
+
 # 🔥 TEMPORAL: Botón para borrar saldo del día 15
 if st.button("🗑️ Borrar saldo 15"):
     db.eliminar_saldo("2026-06-15")
